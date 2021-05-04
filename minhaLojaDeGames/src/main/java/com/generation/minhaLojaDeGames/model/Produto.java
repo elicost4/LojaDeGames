5ce89a7f6a5f843802bbd14642749c34946e1176
elicost4 <eliabecostaa@outlook.com>
2021-05-04 19:55:01 -0300

    add relacionamento

---
+++ minhaLojaDeGames/src/main/java/com/generation/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private int codigo;
+	
+	private double preco;
+	
+	private boolean ativo;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
